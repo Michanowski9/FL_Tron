2a33213de9698baf806082b67a05dcfaf5779c4a
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2022-05-25 20:33:51 +0200

    Delete FL_TRON_WINDOWS directory

--- FL_TRON_WINDOWS/FL_TRON/stringFuncs.h
+++
@@ -1,9 +0,0 @@
-
-//zwraca indeks nastepnej spacji, jesli since wskazywuje na spacje
-//to zwroci indeks nastepnej
-int getNextSpaceBar(char* text, int since);
-
-//zwraca indeks spacji po tym slowie
-int getNextNick(char*input,char* output, int since);
-
-
